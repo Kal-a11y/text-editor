ed797689f01e409ef1d076597038dea149860fe8
Kaila <kailaamia@gmail.com>
2024-02-08 16:16:33 -0600

    Added database crud logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.put(content);
+  const res = await req;
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log('putDb result is', res);
+  return res;
+}
 
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+
+  console.log('getDb result is', res);
+  return res;
+  
+}
 initdb();
