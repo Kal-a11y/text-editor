4f67765d7559a5bda58e6c1e75822f4de8cd830c
Kaila <kailaamia@gmail.com>
2024-02-08 16:55:20 -0600

    Fixed add

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const req = store.add(content);
+  const req = store.add({ todo: content });
   const res = await req;
 
   console.log('putDb result is', res);
